503500576d9ba396ccb8d0d7b3ec1c5056202ff6
Denys <hetfield228@gmail.com>
2024-10-06 20:52:23 +0300

    Code improvements

--- todo.py
+++ todo.py
@@ -1,24 +1,45 @@
-todo_list = []
 while True:
-    todo_input = input("Enter add, show, edit or exit: ")
+    todo_input = input("Enter add, show, edit, complete or exit: ")
     todo_input = todo_input.strip()
 
     match todo_input:
         case "add":
-            todo = input("Enter a todo: ")
-            todo_list.append(todo.capitalize())
+            todo = input("Enter a todo: ") + '\n'
+
+            with open('todos.txt', 'a') as txt_file:
+                txt_file.writelines(todo)
         case "show":
-            a = 0
-            for item in todo_list:
-                a += 1
-                print(f"{a}.", item)
+            with open('todos.txt', 'r') as txt_file:
+                for index, item in enumerate(txt_file, start=1):
+                    print(f"{index}. {item}".strip('\n'))
         case "edit":
             edit = int(input("Ok! Write the todo`s number you want to edit: ")) - 1
-            edited_todo = input("Got it! Write new todo: ")
+            edited_todo = input("Got it! Write new todo: ") + "\n"
+
+            with open('todos.txt', 'r') as txt_file:
+                todo_list = txt_file.readlines()
+
             todo_list[edit] = edited_todo
+
+            with open('todos.txt', 'w') as txt_file:
+                txt_file.writelines(todo_list)
+        case "complete":
+            complete = int(input("Write the todo`s number that you completed: ")) - 1
+
+            with open('todos.txt', 'r') as txt_file:
+                todo_list = txt_file.readlines()
+
+            completed_todo = todo_list[complete]
+            todo_list.pop(complete)
+
+            with open('todos.txt', 'w') as txt_file:
+                txt_file.writelines(todo_list)
+
+            message = f'Congratulations! Todo "{completed_todo.strip('\n')}" was removed from your list.'
+            print(message)
         case "exit":
             break
         case _:
-            print("Wrong number.")
+            print("Try again.")
 
 print("Good luck with all of this shitty tasks!")
