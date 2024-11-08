f8aebd6f71f6495d6da03e4017894f4127ef8df9
Denys <hetfield228@gmail.com>
2024-11-08 23:25:55 +0200

    Code improvements

--- todo.py
+++ todo.py
@@ -1,45 +1,58 @@
+import time
+
+import functions
+
+now = time.strftime("%b %d, %Y - %H:%M:%S")
+print(f"It is {now} now.")
 while True:
     todo_input = input("Enter add, show, edit, complete or exit: ")
     todo_input = todo_input.strip()
 
-    match todo_input:
-        case "add":
-            todo = input("Enter a todo: ") + '\n'
+    if todo_input.startswith("add"):
+        todo = todo_input[4:] + '\n'
 
-            with open('todos.txt', 'a') as txt_file:
-                txt_file.writelines(todo)
-        case "show":
-            with open('todos.txt', 'r') as txt_file:
-                for index, item in enumerate(txt_file, start=1):
-                    print(f"{index}. {item}".strip('\n'))
-        case "edit":
-            edit = int(input("Ok! Write the todo`s number you want to edit: ")) - 1
+        with open('todos.txt', 'a') as txt_file:
+            txt_file.writelines(todo)
+
+    elif todo_input.startswith("show"):
+        with open('todos.txt', 'r') as txt_file:
+            for index, item in enumerate(txt_file, start=1):
+                print(f"{index}. {item}".strip('\n'))
+                
+    elif todo_input.startswith("edit"):
+        try:
+            edit = int(todo_input[5:]) - 1
             edited_todo = input("Got it! Write new todo: ") + "\n"
 
-            with open('todos.txt', 'r') as txt_file:
-                todo_list = txt_file.readlines()
+            todo_list = functions.read_file()
 
             todo_list[edit] = edited_todo
 
-            with open('todos.txt', 'w') as txt_file:
-                txt_file.writelines(todo_list)
-        case "complete":
-            complete = int(input("Write the todo`s number that you completed: ")) - 1
+            functions.write_file(todo_list)
+        except ValueError:
+            print("Your command is not valid.")
+            continue
+    elif todo_input.startswith("complete"):
+        try:
+            complete = int(todo_input[9:]) - 1
 
-            with open('todos.txt', 'r') as txt_file:
-                todo_list = txt_file.readlines()
+            todo_list = functions.read_file()
 
             completed_todo = todo_list[complete]
             todo_list.pop(complete)
 
-            with open('todos.txt', 'w') as txt_file:
-                txt_file.writelines(todo_list)
+            functions.write_file(todo_list)
 
             message = f'Congratulations! Todo "{completed_todo.strip('\n')}" was removed from your list.'
             print(message)
-        case "exit":
-            break
-        case _:
-            print("Try again.")
-
+        except ValueError:
+            print("Your command is not valid.")
+            continue
+        except IndexError:
+            print("Number is out of list range. Try again!")
+            continue
+    elif todo_input.startswith("exit"):
+        break
+    else:
+        print("Try again.")
 print("Good luck with all of this shitty tasks!")
